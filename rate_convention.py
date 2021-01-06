c519b03707a07730d82e3d52eb68b20adc625007
nicofish <64016872+nicofish@users.noreply.github.com>
2021-01-06 14:42:06 +0100

    Add files via upload

--- rate_convention.py
+++ rate_convention.py
@@ -1,5 +1,5 @@
 # Object  : Discount factor, capitalisation factor, rate convention, rate conversion
-# Version : v1.0
+# Version : v1.1
 # Date    : 2021.01.06
 # Python  : v3.7.7
 # Author  : NicoFish
@@ -7,42 +7,65 @@
 from math import exp, log
 import string
 
-def discount_factor(rate_convention, rate, time):
-    """
-    Compute discount factor based on parameters :
-    - rate convention : exponential, yield, linear, discount
-    - rate : interest rate. For instance r = 0.035 for 3.5 %
-    - time : time in year. For instance t = 0.75 for 9 months
-    """
-    rate_convention = rate_convention.lower()
-    if rate_convention in [ "exp", "exponential" ] :
-        df = exp( rate * time )
-    elif rate_convention in [ "yld", "yield" ] :
-        df = (1 + rate ) ** time
-    elif rate_convention in [ "lin", "linear" ] :
-        df = 1 + rate * time
-    elif rate_convention in [ "dsc", "discount" ] :
-        if rate *  time != 1 :
-            df = 1 / (1 - rate *  time)
-        else :
-            df = None
-    else :
-        df = None
-    return df
-
-def capitalisation_factor(rate_convention, rate, time) :
+def capitalisation_factor(rate_convention, rate, time):
     """
     Compute capitalisation factor based on parameters :
     - rate convention : exponential, yield, linear, discount
     - rate : interest rate. For instance r = 0.035 for 3.5 %
     - time : time in year. For instance t = 0.75 for 9 months
     """
-    df = discount_factor(rate_convention, rate, time)
-    if df != None and df != 0 :
-        cf = 1 / df
+    rate_convention = rate_convention.lower()
+    if rate_convention in [ "exp", "exponential" ] :
+        cf = exp( rate * time )
+    elif rate_convention in [ "yld", "yield" ] :
+        cf = (1 + rate ) ** time
+    elif rate_convention in [ "lin", "linear" ] :
+        cf = 1 + rate * time
+    elif rate_convention in [ "dsc", "discount" ] :
+        if rate *  time != 1 :
+            cf = 1 / (1 - rate *  time)
+        else :
+            cf = None
     else :
         cf = None
     return cf
+
+def discount_factor(rate_convention, rate, time) :
+    """
+    Compute discount factor based on parameters :
+    - rate convention : exponential, yield, linear, discount
+    - rate : interest rate. For instance r = 0.035 for 3.5 %
+    - time : time in year. For instance t = 0.75 for 9 months
+    """
+    cf = capitalisation_factor(rate_convention, rate, time)
+    if cf != None and cf != 0 :
+        df = 1 / cf
+    else :
+        df = None
+    return df
+
+def capitalisationfactor_to_rate(rate_convention, capitalisation_factor, time):
+    """
+    Convert capitalisation factor to interest rate.
+    Parameters are :
+    - rate convention : exponential, yield, linear, capitalisation
+    - capitalisation factor
+    - time : time in year. For instance t = 0.75 for 9 months
+    """
+    rate_convention = rate_convention.lower()
+    if rate_convention in [ "exp", "exponential" ] and capitalisation_factor >= 0.0 and time != 0 :
+        rate = log(capitalisation_factor) / time
+    elif rate_convention in [ "yield", "yld" ] and time != 0 :
+        rate = capitalisation_factor ** (1/time) - 1
+    elif rate_convention in [ "linear", "lin" ] and time != 0 :
+        rate = (capitalisation_factor - 1) / time
+    elif rate_convention in [ "capitalisation", "dsc" ] and capitalisation_factor != 0 and time != 0:
+        rate = (1 - 1/capitalisation_factor) / time
+    else :
+        rate = None
+    if rate != None:
+        rate = round(rate,15)
+    return rate
 
 def discountfactor_to_rate(rate_convention, discount_factor, time):
     """
@@ -52,32 +75,9 @@ def discountfactor_to_rate(rate_convention, discount_factor, time):
     - discount factor
     - time : time in year. For instance t = 0.75 for 9 months
     """
-    rate_convention = rate_convention.lower()
-    if rate_convention in [ "exp", "exponential" ] and discount_factor >= 0.0 and time != 0 :
-        rate = log(discount_factor) / time
-    elif rate_convention in [ "yield", "yld" ] and time != 0 :
-        rate = discount_factor ** (1/time) - 1
-    elif rate_convention in [ "linear", "lin" ] and time != 0 :
-        rate = (discount_factor - 1) / time
-    elif rate_convention in [ "discount", "dsc" ] and discount_factor != 0 and time != 0:
-        rate = (1 - 1/discount_factor) / time
-    else :
-        rate = None
-    if rate != None:
-        rate = round(rate,15)
-    return rate
-
-def capitalisationfactor_to_rate(rate_convention, capitalisation_factor, time):
-    """
-    Convert capitalisation factor to interest rate.
-    Parameters are :
-    - rate convention : exponential, yield, linear, discount
-    - discount factor
-    - time : time in year. For instance t = 0.75 for 9 months
-    """
-    if capitalisation_factor != 0:
-        discount_factor = 1 / capitalisation_factor
-        rate = discountfactor_to_rate(rate_convention, discount_factor, time)
+    if discount_factor != 0:
+        capitalisation_factor = 1 / discount_factor
+        rate = capitalisationfactor_to_rate(rate_convention, capitalisation_factor, time)
     else:
         rate = None
     return rate
